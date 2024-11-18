e07dcf9067617ff7588ad4c52043b6109af57731
Astra <abhaypratapsinghwd@gmail.com>
2024-11-18 18:39:00 +0530

    Initial SetUp

---
+++ components/providers/provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+// import { SessionProvider } from "next-auth/react";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Providers = ({ children }: Props) => {
+  return (
+    // <SessionProvider>
+    <div>{children}</div>
+    // </SessionProvider>
+  );
+};
+
+export default Providers;
---
+++ config/site.config.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types/site.config";
+
+export const siteConfig: SiteConfig = {
+  name: "SkillX",
+  description:
+    "A centralized platform connecting universities, corporates, and students for seamless campus placement and employability insights.",
+  url: "https://skillx.vercel.app",
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    github: "https://github.com/SinghAstra/SkillX",
+    linkedIn: "https://www.linkedin.com/in/singhastra",
+  },
+};
