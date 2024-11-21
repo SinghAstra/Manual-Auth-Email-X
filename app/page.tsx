9368f03835009b8cf418ada17b8eeb6f9c276a55
Astra <abhaypratapsinghwd@gmail.com>
2024-11-21 14:28:05 +0530

    Auth Deployment Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <h1 className="min-h-screen bg-black text-white flex items-center justify-center">
-      Hey There
+      Hey There Raj Singh
     </h1>
   );
 }
