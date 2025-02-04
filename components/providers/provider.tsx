83aaaf2c250bf4a041acb8a0b2f7bd85aea90f49
Astra <abhaypratapsinghwd@gmail.com>
2025-02-04 18:31:26 +0530

    Added Layout.tsx

---
+++ components/providers/provider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+// import { SessionProvider } from "next-auth/react";
+import React, { Suspense } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const LoadingFallback = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      This is Loading Fallback You Specified in components/providers/provider
+    </div>
+  );
+};
+
+const Providers = ({ children }: Props) => {
+  return (
+    <Suspense fallback={<LoadingFallback />}>
+      {/* <SessionProvider> */}
+      {children}
+      {/* </SessionProvider> */}
+    </Suspense>
+  );
+};
+
+export default Providers;
