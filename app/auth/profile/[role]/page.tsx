17a08bc63f8720a121d3eb8eb6c1764784c4cb64
Astra <abhaypratapsinghwd@gmail.com>
2025-02-17 09:48:24 +0530

    Company Rep Form

--- app/auth/profile/[role]/page.tsx
+++ app/auth/profile/[role]/page.tsx
@@ -25,18 +25,24 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { formatDocumentType } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { DocumentType } from "@prisma/client";
 import { X } from "lucide-react";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import { FaSpinner } from "react-icons/fa";
 import * as z from "zod";
 
 enum InstitutionAdminDocumentsType {
   INSTITUTION_ID = "INSTITUTION_ID",
   AUTHORIZATION_LETTER = "AUTHORIZATION_LETTER",
+}
+
+enum CompanyRepresentativeDocumentsType {
+  COMPANY_ID = "COMPANY_ID",
+  BUSINESS_CARD = "BUSINESS_CARD",
 }
 
 interface DocumentFile {
@@ -49,7 +55,12 @@ type InstitutionAdminDocumentsFiles = {
   [key in InstitutionAdminDocumentsType]?: DocumentFile;
 };
 
+type CompanyRepresentativeDocumentsFiles = {
+  [key in CompanyRepresentativeDocumentsType]?: DocumentFile;
+};
+
 const InstitutionAdminForm = () => {
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
   const fileInputRefs = useRef<
     Record<InstitutionAdminDocumentsType, HTMLInputElement | null>
   >({
@@ -58,8 +69,12 @@ const InstitutionAdminForm = () => {
   });
 
   const [documents, setDocuments] = useState<InstitutionAdminDocumentsFiles>({
-    [DocumentType.INSTITUTION_ID]: { file: null, preview: null, error: null },
-    [DocumentType.AUTHORIZATION_LETTER]: {
+    [InstitutionAdminDocumentsType.INSTITUTION_ID]: {
+      file: null,
+      preview: null,
+      error: null,
+    },
+    [InstitutionAdminDocumentsType.AUTHORIZATION_LETTER]: {
       file: null,
       preview: null,
       error: null,
@@ -138,9 +153,7 @@ const InstitutionAdminForm = () => {
     address: z.string().min(10),
     city: z.string().min(2),
     state: z.string().min(2),
-    pincode: z.string().length(6),
     website: z.string().url().optional(),
-    phone: z.string().min(10),
   });
 
   const form = useForm({
@@ -151,18 +164,9 @@ const InstitutionAdminForm = () => {
       address: "",
       city: "",
       state: "",
-      pincode: "",
       website: "",
-      phone: "",
     },
   });
-
-  const formatDocumentType = (type: string): string => {
-    return type
-      .split("_")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-      .join(" ");
-  };
 
   const onSubmit = (data: z.infer<typeof formSchema>) => {
     console.log(data);
@@ -172,11 +176,13 @@ const InstitutionAdminForm = () => {
     if (!isValidDocuments) {
       return;
     }
+
+    setIsFormSubmitting(true);
     // Handle form submission
   };
 
   return (
-    <div className="w-full max-w-xl border rounded-md py-2 px-4">
+    <div className="w-full max-w-xl border rounded-md py-2 px-4 mt-4">
       <div className="mb-4">
         <h2 className="text-2xl">Institution Administrator Profile</h2>
         <span className="text-sm text-muted-foreground">
@@ -275,37 +281,6 @@ const InstitutionAdminForm = () => {
             />
           </div>
 
-          <div className="grid grid-cols-2 gap-4">
-            <FormField
-              control={form.control}
-              name="pincode"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-sm transition-colors font-normal">
-                    Pincode
-                  </FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter 6-digit pincode" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="phone"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-sm transition-colors font-normal">
-                    Phone Number
-                  </FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter phone number" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
-
           <FormField
             control={form.control}
             name="website"
@@ -381,8 +356,14 @@ const InstitutionAdminForm = () => {
             </div>
           ))}
 
-          <Button type="submit" className="w-full">
-            Submit
+          <Button type="submit" className="w-full" disabled={isFormSubmitting}>
+            {isFormSubmitting ? (
+              <>
+                <FaSpinner className="animate-spin" /> Wait ....
+              </>
+            ) : (
+              "Submit"
+            )}
           </Button>
         </form>
       </Form>
@@ -391,90 +372,286 @@ const InstitutionAdminForm = () => {
 };
 
 const CompanyRepresentativeForm = () => {
+  const [isFormSubmitting, setIsFormSubmitting] = useState(false);
+
+  const fileInputRefs = useRef<
+    Record<CompanyRepresentativeDocumentsType, HTMLInputElement | null>
+  >({
+    [CompanyRepresentativeDocumentsType.COMPANY_ID]: null,
+    [CompanyRepresentativeDocumentsType.BUSINESS_CARD]: null,
+  });
+
+  const [documents, setDocuments] =
+    useState<CompanyRepresentativeDocumentsFiles>({
+      [CompanyRepresentativeDocumentsType.COMPANY_ID]: {
+        file: null,
+        preview: null,
+        error: null,
+      },
+      [CompanyRepresentativeDocumentsType.BUSINESS_CARD]: {
+        file: null,
+        preview: null,
+        error: null,
+      },
+    });
+
+  const handleFileChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    type: CompanyRepresentativeDocumentsType
+  ) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const previewUrl = URL.createObjectURL(file);
+      setDocuments((prev) => ({
+        ...prev,
+        [type]: {
+          file,
+          preview: previewUrl,
+          error: null,
+        },
+      }));
+    }
+  };
+
+  const removeDocument = (type: CompanyRepresentativeDocumentsType) => {
+    const currentPreview = documents[type]?.preview;
+    if (currentPreview) {
+      URL.revokeObjectURL(currentPreview);
+    }
+
+    setDocuments((prev) => ({
+      ...prev,
+      [type]: {
+        file: null,
+        preview: null,
+        error: null,
+      },
+    }));
+
+    if (fileInputRefs.current[type]) {
+      fileInputRefs.current[type].value = "";
+    }
+  };
+
+  const validateDocuments = (): boolean => {
+    let isValid = true;
+
+    Object.values(CompanyRepresentativeDocumentsType).forEach((type) => {
+      if (!documents[type]?.file) {
+        setDocuments((prev) => ({
+          ...prev,
+          [type]: {
+            file: null,
+            preview: null,
+            error: `${formatDocumentType(type)} is required`,
+          },
+        }));
+        isValid = false;
+      } else {
+        setDocuments((prev) => ({
+          ...prev,
+          [type]: {
+            ...documents[type],
+            error: null,
+          },
+        }));
+      }
+    });
+
+    console.log("isValid is ", isValid);
+    console.log("documents is ", documents);
+
+    return isValid;
+  };
+
   const formSchema = z.object({
     companyName: z.string().min(2),
-    industry: z.string(),
-    designation: z.string(),
-    department: z.string().optional(),
-    website: z.string().url().optional(),
+    website: z.union([z.literal(""), z.string().trim().url()]),
     address: z.string(),
     city: z.string(),
     state: z.string(),
-    pincode: z.string().length(6),
   });
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       companyName: "",
-      industry: "",
-      designation: "",
-      department: "",
       website: "",
       address: "",
       city: "",
       state: "",
-      pincode: "",
     },
   });
 
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+    // Validate documents
+    const isValidDocuments = validateDocuments();
+
+    if (!isValidDocuments) {
+      return;
+    }
+
+    setIsFormSubmitting(true);
+    // Handle form submission
+  };
+
   return (
-    <Card className="w-full max-w-2xl">
-      <CardHeader>
-        <CardTitle>Company Representative Profile</CardTitle>
-        <CardDescription>Provide your company and role details</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form className="space-y-4">
+    <div className="w-full max-w-xl border rounded-md py-2 px-4 mt-4">
+      <div className="mb-4">
+        <h2 className="text-2xl">Company Representative Profile</h2>
+        <span className="text-sm text-muted-foreground">
+          Provide your company and role details
+        </span>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="companyName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-sm transition-colors font-normal">
+                  Company Name
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter company name" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-sm transition-colors font-normal">
+                  Address
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter complete address" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-2 gap-4">
             <FormField
               control={form.control}
-              name="companyName"
+              name="city"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Company Name</FormLabel>
+                  <FormLabel className="text-sm transition-colors font-normal">
+                    City
+                  </FormLabel>
                   <FormControl>
-                    <Input placeholder="Enter company name" {...field} />
+                    <Input placeholder="Enter city" {...field} />
                   </FormControl>
-                  <FormMessage />
                 </FormItem>
               )}
             />
-            <div className="grid grid-cols-2 gap-4">
-              <FormField
-                control={form.control}
-                name="designation"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Designation</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="department"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Department</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+            <FormField
+              control={form.control}
+              name="state"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-sm transition-colors font-normal">
+                    State
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter state" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name="website"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-sm transition-colors font-normal">
+                  Website (Optional)
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter institution website" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          {Object.values(CompanyRepresentativeDocumentsType).map((type) => (
+            <div key={type}>
+              <FormLabel
+                className={`text-sm transition-colors font-normal ${
+                  documents[type] && documents[type].error
+                    ? "text-destructive"
+                    : ""
+                }`}
+              >
+                {formatDocumentType(type)}
+              </FormLabel>
+              <div className="mt-2">
+                <Input
+                  type="file"
+                  accept="image/*,.pdf"
+                  onChange={(e) => handleFileChange(e, type)}
+                  className={`w-full cursor-pointer ${
+                    documents[type] && documents[type].error
+                      ? "border-destructive"
+                      : ""
+                  }`}
+                  ref={(el) => {
+                    fileInputRefs.current[type] = el;
+                  }}
+                />
+              </div>
+              {documents[type]?.preview && (
+                <div className="mt-2 relative">
+                  <div className="relative w-full h-40 border rounded-md overflow-hidden">
+                    {documents[type]?.file?.type.startsWith("image/") ? (
+                      <Image
+                        src={documents[type]?.preview}
+                        alt={`${type} Preview`}
+                        width={320}
+                        height={160}
+                        className="w-full h-full object-contain"
+                      />
+                    ) : (
+                      <div className="w-full h-full flex items-center justify-center bg-muted">
+                        <p className="text-sm text-muted-foreground">
+                          PDF Document
+                        </p>
+                      </div>
+                    )}
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      onClick={() =>
+                        removeDocument(
+                          type as CompanyRepresentativeDocumentsType
+                        )
+                      }
+                      className="absolute top-2 right-2 p-1 bg-background rounded-full shadow-md hover:bg-accent transition-colors"
+                    >
+                      <X className="h-4 w-4 text-foreground" />
+                    </Button>
+                  </div>
+                </div>
+              )}
             </div>
-            <Button type="submit" className="w-full">
-              Submit
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+          ))}
+          <Button type="submit" className="w-full" disabled={isFormSubmitting}>
+            {isFormSubmitting ? (
+              <>
+                <FaSpinner className="animate-spin" /> Wait ....
+              </>
+            ) : (
+              "Submit"
+            )}
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
